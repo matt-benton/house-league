6a0ae0ace1971edbaf44fba9c3b52706abf6dfeb
Matt Benton <mattbenton@hey.com>
2026-07-05 09:42:34 -0500

    Add actions menu to dashboard

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -1,5 +1,30 @@
 <div>
-    <flux:heading size="xl" class="mb-3">News</flux:heading>
+    @if(auth()->user()?->is_admin)
+        <flux:heading size="xl" class="mb-3">Actions</flux:heading>
+        <flux:card class="space-y-2">
+            <flux:button
+                variant="primary"
+                href="/posts/create"
+                icon="pencil"
+                wire:navigate
+                size="sm"
+                class="w-full"
+            >
+                New Blog Post
+            </flux:button>
+            <flux:button
+                variant="primary"
+                href="/matches/create"
+                icon="ticket"
+                wire:navigate
+                size="sm"
+                class="w-full"
+            >
+                New Match
+            </flux:button>
+        </flux:card>
+    @endif
+    <flux:heading size="xl" class="my-3">News</flux:heading>
     @if ($this->posts->isNotEmpty())
         <flux:card class="space-y-11">
             @foreach ($this->posts as $post)
