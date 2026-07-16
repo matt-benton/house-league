b447e31c569341cce83dc44f42bd2b836da01e71
Matt Benton <mattbenton@hey.com>
2026-07-16 15:21:38 -0500

    Fix seeder to run in production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'is_admin' => 1,
-        ]);
+        if (config('app.env' === 'local')) {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'is_admin' => 1,
+            ]);
+        }
 
         $this->call([
             TeamSeeder::class,
