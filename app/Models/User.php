976d94c402b406a9fcf8fb1a69f6f519ce1a9640
Matt Benton <mattbenton@hey.com>
2026-07-16 11:08:26 -0500

    Cast user is_admin to boolean

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,7 @@ class User extends Authenticatable implements PasskeyUser
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
         ];
     }
 
