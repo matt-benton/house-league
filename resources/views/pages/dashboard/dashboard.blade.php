633c98e57c0bd1e9ba21ac10418f31673a71f253
Matt Benton <mattbenton@hey.com>
2026-07-31 09:02:36 -0500

    Show headlines but no body text on dashboard

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -47,22 +47,13 @@
 
     <flux:heading size="xl" class="mb-3 mt-5">Headlines</flux:heading>
     @if ($this->posts->isNotEmpty())
-        <flux:card class="space-y-11">
-            @foreach ($this->posts as $post)
-                <div>
-                    <flux:heading size="lg" class="mb-3">
-                        <flux:link href="/posts/{{ $post->id }}" wire:navigate>{{ $post->title }}</flux:link>
-                    </flux:heading>
-                    <flux:text variant="subtle">Published {{ $post->created_at->diffForHumans() }}</flux:text>
-                    <div class="space-y-3">
-                        <flux:text>{!! $post->text !!}</flux:text>
-                    </div>
-                </div>
-                <flux:separator />
-            @endforeach
+        @foreach ($this->posts as $post)
+            <flux:heading size="lg" class="mb-3">
+                <flux:link href="/posts/{{ $post->id }}" wire:navigate>{{ $post->title }}</flux:link>
+            </flux:heading>
+        @endforeach
 
-            <flux:pagination :paginator="$this->posts" />
-        </flux:card>
+        <flux:pagination :paginator="$this->posts" />
     @else
         <flux:callout icon="clock" color="lime">
             <flux:callout.heading>Stay tuned</flux:callout.heading>
