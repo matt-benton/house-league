5791b585e763d96132dad33dab3aea910dac001b
Matt Benton <mattbenton@hey.com>
2026-08-14 17:06:22 -0500

    Display players in a table

--- resources/views/pages/player/index/index.blade.php
+++ resources/views/pages/player/index/index.blade.php
@@ -1,21 +1,75 @@
-<div class="max-w-md mx-auto">
+<div class="mx-auto">
     <flux:heading size="lg" class="mb-3">Players</flux:heading>
-    <ul class="space-y-3">
-        @foreach ($players as $player)
-            <flux:card class="space-y-3">
-                <flux:heading size="lg">
-                    <flux:link href="/players/{{ $player->id }}" variant="ghost" wire:navigate>{{ $player->name }}</flux:link>
-                </flux:heading>
-                <flux:text>{{ $player->number }} - {{ ucfirst($player->position->value) }}</flux:text>
-                <flux:text>
-                    @if ($player->team_id)
-                        <flux:link href="/teams/{{ $player->team_id }}" variant="ghost" wire:navigate>{{ $player->team->name }}</flux:link>
-                    @else
-                        No Team
-                    @endif
-                </flux:text>
-            </flux:card>
-        @endforeach
-    </ul>
+        <flux:table :paginate="$this->players">
+            <flux:table.columns>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'name'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('name')"
+                    sticky
+                    class="bg-zinc-800"
+                >
+                    Name
+                </flux:table.column>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'team_name'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('team_name')"
+                >
+                    Team
+                </flux:table.column>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'goals_count'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('goals_count')"
+                >
+                    Goals
+                </flux:table.column>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'saves_count'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('saves_count')"
+                >
+                    Saves
+                </flux:table.column>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'yellow_cards_count'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('yellow_cards_count')"
+                >
+                    Yellow Cards
+                </flux:table.column>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'red_cards_count'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('red_cards_count')"
+                >
+                    Red Cards
+                </flux:table.column>
+            </flux:table.columns>
+
+            @foreach ($this->players as $player)
+                <flux:table.rows wire:key="$player->id">
+                    <flux:table.row>
+                        <flux:table.cell sticky class="bg-zinc-800">
+                            <flux:link variant="ghost" href="/players/{{ $player->id }}" wire:navigate>
+                                {{ $player->name }}
+                            </flux:link>
+                        </flux:table.cell>
+                        <flux:table.cell>{{ $player->team_name }}</flux:table.cell>
+                        <flux:table.cell>{{ $player->goals_count }}</flux:table.cell>
+                        <flux:table.cell>{{ $player->saves_count }}</flux:table.cell>
+                        <flux:table.cell>{{ $player->yellow_cards_count }}</flux:table.cell>
+                        <flux:table.cell>{{ $player->red_cards_count }}</flux:table.cell>
+                    </flux:table.row>
+                </flux:table.rows>
+            @endforeach
+        </flux:table>
     <flux:button href="/players/create" class="mt-6 float-end" variant="primary" icon="user" wire:navigate>New Player</flux:button>
 </div>
--- resources/views/pages/player/index/index.php
+++ resources/views/pages/player/index/index.php
@@ -1,17 +1,52 @@
 <?php
 
 use App\Models\Player;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 new #[Title('Players')] class extends Component
 {
-    public $players;
+    use WithPagination;
+
+    public string $sortBy = 'name';
+
+    public string $sortDirection = 'asc';
 
     public function mount()
     {
         $this->authorize('viewAny', Player::class);
+    }
 
-        $this->players = Player::all();
+    #[Computed]
+    public function players()
+    {
+        return Player::query()
+            ->select(
+                'players.*',
+                'teams.name as team_name',
+            )
+            ->leftJoin('teams', 'teams.id', '=', 'players.team_id')
+            ->withCount('goals', 'saves', 'redCards', 'yellowCards')
+            ->when($this->sortBy, fn (Builder $query) => $query->orderBy($this->sortBy, $this->sortDirection))
+            ->paginate();
+    }
+
+    public function setSortBy(string $field)
+    {
+        $this->toggleSortDirection();
+
+        $this->sortBy = $field;
+    }
+
+    private function toggleSortDirection()
+    {
+        if ($this->sortDirection === 'asc') {
+            $this->sortDirection = 'desc';
+        } else {
+            $this->sortDirection = 'asc';
+        }
     }
 };
