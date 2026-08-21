6ce8317cd7ab58db765050097c018dc490c15088
Matt Benton <mattbenton@hey.com>
2026-08-21 15:33:06 -0500

    Change the teams list to a table

--- resources/views/pages/team/index/index.blade.php
+++ resources/views/pages/team/index/index.blade.php
@@ -1,15 +1,60 @@
-<div class="max-w-md mx-auto">
+<div class="max-w-lg mx-auto">
     <flux:heading size="lg" class="mb-3">Teams</flux:heading>
-    <ul class="space-y-3">
-        @foreach ($teams as $team)
-            <li>
-                <flux:card class="flex items-baseline gap-2">
-                    <flux:link href="/teams/{{ $team->id }}" variant="ghost">{{ $team->name }}</flux:link>
-                    <flux:text>({{ $team->abbreviation }})</flux:text>
-                    <flux:text>{{ $team->record }}</flux:text>
-                </flux:card>
-            </li>
-        @endforeach
-    </ul>
+        <flux:table :paginate="$this->teams">
+            <flux:table.columns>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'name'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('name')"
+                    sticky
+                    class="bg-white dark:bg-zinc-800"
+                >
+                    Name
+                </flux:table.column>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'wins'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('wins')"
+                    align="end"
+                >
+                    Wins
+                </flux:table.column>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'losses'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('losses')"
+                    align="end"
+                >
+                    Losses
+                </flux:table.column>
+                <flux:table.column
+                    sortable
+                    :sorted="$sortBy === 'draws'"
+                    :direction="$sortDirection"
+                    wire:click="setSortBy('draws')"
+                    align="end"
+                >
+                    Draws
+                </flux:table.column>
+            </flux:table.columns>
+
+            @foreach ($this->teams as $team)
+                <flux:table.rows wire:key="$team->id">
+                    <flux:table.row>
+                        <flux:table.cell sticky class="bg-white dark:bg-zinc-800">
+                            <flux:link variant="ghost" href="/teams/{{ $team->id }}" wire:navigate>
+                                {{ $team->name }}
+                            </flux:link>
+                        </flux:table.cell>
+                        <flux:table.cell align="end">{{ $team->wins }}</flux:table.cell>
+                        <flux:table.cell align="end">{{ $team->losses }}</flux:table.cell>
+                        <flux:table.cell align="end">{{ $team->draws }}</flux:table.cell>
+                    </flux:table.row>
+                </flux:table.rows>
+            @endforeach
+        </flux:table>
     <flux:button href="/teams/create" class="mt-6 float-end" variant="primary" icon="flag" wire:navigate>New Team</flux:button>
 </div>
--- resources/views/pages/team/index/index.php
+++ resources/views/pages/team/index/index.php
@@ -1,19 +1,56 @@
 <?php
 
 use App\Models\Team;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 new #[Title('Teams')] class extends Component
 {
-    public $teams;
+    use WithPagination;
 
-    public $foo;
+    public string $sortBy = 'name';
+
+    public string $sortDirection = 'asc';
 
     public function mount()
     {
         $this->authorize('viewAny', Team::class);
+    }
 
-        $this->teams = Team::all();
+    #[Computed]
+    public function teams()
+    {
+        return Team::query()
+            ->when($this->sortBy, fn (Builder $query) => $query->orderBy($this->sortBy, $this->sortDirection))
+            ->paginate();
+    }
+
+    public function setSortBy(string $field)
+    {
+        $this->toggleSortDirection($field);
+
+        $this->sortBy = $field;
+    }
+
+    private function toggleSortDirection($field)
+    {
+        if ($this->sortBy != $field) {
+            // the sortBy was changed
+            if (Str::contains($field, ['wins', 'losses', 'draws'])) {
+                $this->sortDirection = 'desc';
+            } else {
+                $this->sortDirection = 'asc';
+            }
+        } else {
+            if ($this->sortDirection === 'desc') {
+                $this->sortDirection = 'asc';
+            } else {
+                $this->sortDirection = 'desc';
+            }
+        }
     }
 };
